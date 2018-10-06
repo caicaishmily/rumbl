3851f5efd1987e7b70b43fb9419cf8b16fa80ded
davechai <davecqx@gmail.com>
2018-10-06 12:48:33 +0800

    add new user changeset

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -2,6 +2,12 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
 
   def index(conn, _params) do
     users = Accounts.list_users()
