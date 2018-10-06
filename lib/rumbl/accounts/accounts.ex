20128b53131fa6a9d4bb082794b82e5bda6455e0
davechai <davecqx@gmail.com>
2018-10-06 12:27:44 +0800

    change account to access database

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -3,21 +3,21 @@ defmodule Rumbl.Accounts do
   The Accounts comntext.
   """
 
-  alias Rumbl.Accounts.User
-
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chirs", username: "chrismccord"}
-    ]
-  end
+  alias Rumbl.Repo
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map -> Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end) end)
+    Repo.get_by(User, params)
+  end
+  
+  def list_user do
+    Repo.all(User)
   end
 end
