1f5c1d712ec6c5064e68f529e63a49dff0e998aa
davechai <davecqx@gmail.com>
2018-10-13 23:30:17 +0800

    hash pass when regist new user

--- lib/rumbl/accounts/credential.ex
+++ lib/rumbl/accounts/credential.ex
@@ -4,9 +4,11 @@ defmodule Rumbl.Accounts.Credential do
 
 
   schema "credentials" do
-    field :" email", :string
+    field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
-    field :user_id, :id
+    # field :user_id, :id
+    belongs_to :user, Rumbl.Accounts.User
 
     timestamps()
   end
@@ -14,8 +16,19 @@ defmodule Rumbl.Accounts.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:" email", :password_hash])
-    |> validate_required([:" email", :password_hash])
-    |> unique_constraint(:" email")
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> unique_constraint(:email)
+    |> put_pass_hash()
   end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
+      _ -> changeset
+    end
+  end
+
 end
