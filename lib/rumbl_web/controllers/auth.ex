c85887aab5c96811268fdb549f1e94d217aa1841
davechai <davecqx@gmail.com>
2018-10-14 23:33:43 +0800

    add logout

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -25,5 +25,9 @@ defmodule RumblWeb.Auth do
       {:error, :not_found} -> {:error, :not_found, conn}
     end    
   end
-  
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
+
 end
