728ad4cb2982f3bc00c5415a40f2162acd90a47d
davechai <davecqx@gmail.com>
2018-11-26 23:48:31 +0800

    phoenix clients with ES6

---
+++ assets/js/video.js
@@ -0,0 +1,27 @@
+import Player from "./player"
+
+let Video =  {
+  init(socket, element) {
+    if(!element) {
+      return
+    }
+    let playerId = element.getAttribute("data-player-id")
+    let videoId = element.getAttribute("data-id")
+
+    socket.connect()
+    Player.init(element.id, playerId, () => {
+     this.onReady(videoId, socket)
+    })
+  },
+
+  onReady(videoId, socket) {
+   let msgContainer = document.getElementById("msg-container")
+   let msgInput = document.getElementById("msg-input")
+   let postbutton = document.getElementById("msg-submit")
+   let vidChannel = socket.channnel("videos:" + videoId)
+
+   // TODO join the vidChannel
+  }
+}
+
+export default Video
