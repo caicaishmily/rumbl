d395c98aa3ec12b9386aad86967997e7d3f3f9d5
davechai <davecqx@gmail.com>
2018-11-28 20:08:29 +0800

    taking our channels for a trial run

--- assets/js/video.js
+++ assets/js/video.js
@@ -20,11 +20,44 @@ let Video =  {
    let postbutton = document.getElementById("msg-submit")
    let vidChannel = socket.channnel("videos:" + videoId)
 
+   postbutton.addEventListener("click", e => {
+     let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+     vidChannel.push("new_annotation", payload)
+     .receive("error", e => console.log(e))
+     msgInput.value = ""
+   })
+
+   vidChannel.on("new_annotation", (resp) => {
+     this.renderAnnotation(msgContainer, resp)
+   })
    // TODO join the vidChannel
    vidChannel.join()
    .receive("ok", resp => console.log("joined the video channel", resp))
    .receive("error", reason => console.log("join failed", reason))
+
+   vidChannel.on("ping", ({count}) => console.log("PING", count))
+  },
+
+  esc(str) {
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    // TODO append annotation to msgContainer
+    let template = document.createElement("div")
+
+    template.innerHTML = `
+      <a href="#" data-seek="${this.esc(at)}">
+        <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+      </a>
+    `
+    msgContainer.appendChild(template)
+
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
+
 }
 
 export default Video
