0209e69ba81228bc50d43ef3d571e71247dd2cca
davechai <davecqx@gmail.com>
2018-12-03 17:03:21 +0800

    socket authentication

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -12,10 +12,11 @@ defmodule RumblWeb.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
+
       user = user_id && Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
-      true -> 
+        put_current_user(conn, user)
+      true ->
         assign(conn, :current_user, nil)
     end
   end
@@ -23,17 +24,17 @@ defmodule RumblWeb.Auth do
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
       conn
-    else 
+    else
       conn
       |> put_flash(:error, "You must be logged in to access that page")
       |> redirect(to: Routes.page_path(conn, :index))
       |> halt()
     end
   end
-  
+
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -43,11 +44,18 @@ defmodule RumblWeb.Auth do
       {:ok, user} -> {:ok, login(conn, user)}
       {:error, :unauthorized} -> {:error, :unauthorized, conn}
       {:error, :not_found} -> {:error, :not_found, conn}
-    end    
+    end
   end
 
   def logout(conn) do
     configure_session(conn, drop: true)
   end
 
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
+  end
 end
