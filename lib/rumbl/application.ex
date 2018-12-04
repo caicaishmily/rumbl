2e2a0d57a353de97eafbfb90abb2a086f5a62be1
davechai <davecqx@gmail.com>
2018-12-04 14:54:22 +0800

    adding failover

--- lib/rumbl/application.ex
+++ lib/rumbl/application.ex
@@ -14,6 +14,7 @@ defmodule Rumbl.Application do
       RumblWeb.Endpoint,
       # Starts a worker by calling: Rumbl.Worker.start_link(arg)
       # {Rumbl.Worker, arg},
+      {Rumbl.Counter, 5}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
